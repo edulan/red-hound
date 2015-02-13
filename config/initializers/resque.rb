764cb58accbea6be77c3a1f1eeae2376fe21d8b0
Eduardo Lanchares <eduardo.lanchares@redbooth.com>
2015-02-13 18:55:28 +0100

    Add RedisToGo provider

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -22,3 +22,6 @@ end
 Resque.after_fork do
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
+
+uri = URI.parse(ENV['REDISTOGO_URL'])
+Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
