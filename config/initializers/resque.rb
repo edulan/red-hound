a7f297b2e1b44f0a1a728739c7d660cc67582991
Eduardo Lanchares <eduardo.lanchares@redbooth.com>
2015-02-20 18:25:06 +0100

    Fix resque configuration

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,7 +4,9 @@ require "resque-timeout"
 require "resque/failure/redis"
 require "resque/server"
 
-Resque.redis = ENV['REDIS_PROVIDER']
+redis_url = ENV["REDISCLOUD_URL"] || ENV["OPENREDIS_URL"] || ENV["REDISGREEN_URL"] || ENV["REDISTOGO_URL"] || 'localhost:6379'
+uri = URI.parse(redis_url)
+Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 
 Resque.after_fork do
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
