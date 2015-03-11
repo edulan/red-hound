53424a68a58fcddeb3735cc4b3fdcea61de5d181
Eduardo Lanchares <edulanchi@gmail.com>
2015-03-11 21:46:19 +0100

    Fix feature specs

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,7 +32,4 @@ Houndapp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-
-  # Set test adapter for ActiveJob
-  config.active_job.queue_adapter = :test
 end
--- spec/support/helpers/github_api_helper.rb
+++ spec/support/helpers/github_api_helper.rb
@@ -428,6 +428,12 @@ module GithubApiHelper
       "success",
       "Hound has reviewed the changes."
     )
+    stub_status_request(
+      repo_name,
+      sha,
+      "error",
+      "Hound has detected some style violations. Go and fix it."
+    )
   end
 
   def stub_status_request(full_repo_name, sha, state, description)
